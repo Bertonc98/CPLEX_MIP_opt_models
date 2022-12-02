ec23b2b8e69c660d6ef0fb4c4ee785447fb9d77d
Bertonc98 <bertonc98@gmail.com>
2022-12-02 18:35:02 +0100

    Extraction of gap value update

--- exe/gap_extraction.sh
+++ exe/gap_extraction.sh
@@ -5,7 +5,5 @@ for f in ../src/data/basic_models/formatted*.txt;
 	do
 		name=$(awk -F "/" '{print $5}' <<< $f)
 		echo $name
-		cat $f | awk '{if (NF == 1) print "Instance"$0; else if (NF == 2) print $0; else print $8}' >> "gap_"$name
-		
+		cat $f | awk '{if (NF == 1) print "Instance"$0; else if (NF == 2) print $0; else print $8}' >> "../src/data/basic_models/gap_"$name
 	done
-#cat formatted_CPXPARAM_MIP_Cuts_Gomory_results.txt | awk '{if (NF == 1) print "Instance"$0; else if (NF == 2) print $0; else print $8}'
