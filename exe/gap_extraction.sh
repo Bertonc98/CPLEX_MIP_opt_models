50c25b3086a7062a0bf752fb64b4b70dfe1d1daf
Bertonc98 <bertonc98@gmail.com>
2022-12-04 15:42:28 +0100

    gap search over bug fixed

--- exe/gap_extraction.sh
+++ exe/gap_extraction.sh
@@ -5,5 +5,5 @@ for f in ../src/data/basic_models/formatted*.txt;
 	do
 		name=$(awk -F "/" '{print $5}' <<< $f)
 		echo $name
-		cat $f | awk '{if (NF == 1) print "Instance"$0; else if (NF == 2) print $0; else print $8}' >> "../src/data/basic_models/gap_"$name
+		cat $f | awk '{if (NF == 1) print "Instance"$0; else if (NF == 2) print $0; else print $8}' > "../src/data/basic_models/gap_"$name
 	done
