c222c2dd53f8e742f7a5facac66f937cd0ca6f8a
Bertonc98 <bertonc98@gmail.com>
2023-02-10 18:41:55 +0100

    linearized results gap

--- exe/gap_extraction.sh
+++ exe/gap_extraction.sh
@@ -4,5 +4,5 @@
 for f in ../src/data/$1s/formatted*.txt; 
 	do
 		name=$(awk -F "/" '{print $5}' <<< $f)
-		cat $f | awk '{if (NF == 1) print "Instance"$0; else if (NF == 2) print $0; else if($1 != "0+") print $3";"$5";"$6}' > "../src/data/"$1"s/gap_"$name
+		cat $f | awk '{if (NF == 1) print "Instance"$0; else if (NF == 2) print $0; else if($1 != "0+" && $1 != "Node") print $3";"$5";"$6}' > "../src/data/"$1"s/gap_"$name
 	done
