31f40d3da7528c80375e9058189c5bae46a1bbb6
Bertonc98 <bertonc98@gmail.com>
2023-03-06 12:42:03 +0100

    r strong

--- src/stronger_model.cpp
+++ src/stronger_model.cpp
@@ -51,7 +51,7 @@ int main(int argc, char **argv){
 		compute_W_optimal_hyperplane(solution, wl, wu, scale_factor);
 	}
 	else{
-		compute_W(solution, wl, wu, 10);
+		compute_W(solution, wl, wu, 1);
 	}
 	
 	int k = x.getSize();
