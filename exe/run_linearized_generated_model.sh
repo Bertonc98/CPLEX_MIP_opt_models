fd1e19b49fa53ce526e573403bc251db8e410315
Bertonc98 <bertonc98@gmail.com>
2023-03-07 11:50:07 +0100

    r linear

--- exe/run_linearized_generated_model.sh
+++ exe/run_linearized_generated_model.sh
@@ -2,8 +2,13 @@
 #Call by specifying the model name as first parameter
 for i in {1..5}
 	do for k in 50 100 150
-		do 
-		p=$((k/10))
-		./$1 $i 4 $p $p $k
+		do for d in 10 20 30
+			do for w in 10 100 1000
+				do
+				echo "RUNNING: i: " $i " k: " $k " d: " $d " w: " $w	
+				p=$((k/10))
+				./$1 $i 4 $p $k $d $w
+			done;
+		done;
 	done;
 done;
